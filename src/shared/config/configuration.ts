2c1d027a9fd9d0af81a81690070ac4132687c3df
liverforpresent <dailynsteady@gmail.com>
2025-05-06 01:51:31 +0900

    chore: configuration.ts에 kakao 환경변수 정의

--- src/shared/config/configuration.ts
+++ src/shared/config/configuration.ts
@@ -1,5 +1,13 @@
 export default () => ({
+  // Sever
   server: {
     port: parseInt(process.env.PORT || '3000', 10),
+  },
+
+  // Kakao
+  kakao: {
+    clientId: process.env.KAKAO_CLIENT_ID,
+    clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    redirectUri: process.env.KAKAO_REDIRECT_URI,
   },
 });
