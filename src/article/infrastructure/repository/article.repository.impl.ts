333127b1c3b4ca261b742cf50534b00f10854e74
sw_vi.xi <sunwoo0251@gmail.com>
2025-05-06 01:27:45 +0900

    feat: article에 application 디렉토리 생성 및 파일 생성, get.article.list를 위해 article.repository와 impl 수정

--- src/article/infrastructure/repository/article.repository.impl.ts
+++ src/article/infrastructure/repository/article.repository.impl.ts
@@ -3,10 +3,18 @@ import { Article } from 'src/article/domain/entity/article';
 import { ArticleRepository } from 'src/article/domain/repository/article.repository';
 import { ArticleMapper } from '../mapper/article.mapper';
 import { ArticleEntity } from '../orm-entity/article.entity';
+import { Tag } from 'src/tag/domain/entity/tag';
+import { TagMapper } from 'src/tag/infrastructure/mapper/tag.mapper';
 
 export class ArticleRepositoryImpl extends EntityRepository<ArticleEntity> implements ArticleRepository {
   async save(article: Article): Promise<void> {
     const articleEntity = ArticleMapper.toEntity(article);
     await this.em.persistAndFlush(articleEntity);
+  }
+
+  async findByTags(tags: Tag[]): Promise<Article[] | undefined> {
+    const tagEntities = tags.map((tag) => TagMapper.toEntity(tag));
+    const articles = await this.em.find(ArticleEntity, { tags: { $in: tagEntities } });
+    return articles.map((article) => ArticleMapper.toDomain(article));
   }
 }
