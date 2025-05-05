9a2914cd83046366a65cf9b8bf239667a0002042
sw_vi.xi <sunwoo0251@gmail.com>
2025-05-06 01:27:45 +0900

    feat: article에 application 디렉토리 생성 및 파일 생성, get.article.list를 위해 article.repository와 impl 수정

--- src/article/domain/repository/article.repository.ts
+++ src/article/domain/repository/article.repository.ts
@@ -1,7 +1,9 @@
+import { Tag } from 'src/tag/domain/entity/tag';
 import { Article } from '../entity/article';
 
 export interface ArticleRepository {
   save(article: Article): Promise<void>;
+  findByTags(tags: Tag[]): Promise<Article[] | undefined>;
 }
 
 export const ARTICLE_REPOSITORY = Symbol('ARTICLE_REPOSITORY');
