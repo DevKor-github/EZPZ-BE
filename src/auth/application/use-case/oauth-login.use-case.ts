ca9864f6a24eed97749d4fea9867862141446ad6
liverforpresent <dailynsteady@gmail.com>
2025-05-07 22:01:08 +0900

    feat: 카카오 로그인 api에 jwt 토큰 생성 로직 적용 및 쿠키를 통한 전송 구현

--- src/auth/application/use-case/oauth-login.use-case.ts
+++ src/auth/application/use-case/oauth-login.use-case.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { TokenType } from 'src/auth/infrastructure/factory/jwt.factory';
 import { OAuthProviderFactory } from 'src/auth/infrastructure/oauth/oauth-provider.factory';
+import { JwtProvider } from 'src/auth/infrastructure/provider/jwt.provider';
 
 @Injectable()
 export class OAuthLoginUseCase {
   constructor(
     private readonly oAuthProviderFactory: OAuthProviderFactory,
+    private readonly jwtProvider: JwtProvider,
     // private readonly userService: UserService,
   ) {}
 
@@ -12,6 +15,10 @@ export class OAuthLoginUseCase {
     const provider = this.oAuthProviderFactory.getProvider(providerName);
     const token = await provider.getToken(code);
     const userInfo = await provider.getUserInfo(token);
+    const accessToken = await this.jwtProvider.generateToken(TokenType.ACCESS, 1);
+    const refreshToken = await this.jwtProvider.generateToken(TokenType.REFRESH, 1);
     console.log('userInfo', userInfo);
+
+    return { accessToken, refreshToken };
   }
 }
--- src/auth/presentation/auth.controller.ts
+++ src/auth/presentation/auth.controller.ts
@@ -3,6 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { OAuthLoginUseCase } from '../application/use-case/oauth-login.use-case';
 import { Response } from 'express';
 import { AuthorizeOAuthUseCase } from '../application/use-case/authorize-oauth.use-case';
+import { accessTokenCookieOptions, refreshTokenCookieOptions } from 'src/shared/config/cookie.config';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -20,8 +21,13 @@ export class AuthController {
   }
 
   @Get('login/oauth/callback')
-  async oAuthLogin(@Query('code') code: string) {
-    await this.oAuthLoginUseCase.execute('kakao', code);
+  async oAuthLogin(@Query('code') code: string, @Res() res: Response) {
+    const { accessToken, refreshToken } = await this.oAuthLoginUseCase.execute('kakao', code);
+
+    res.cookie('accessToken', accessToken, accessTokenCookieOptions);
+    res.cookie('refreshToken', refreshToken, refreshTokenCookieOptions);
+
+    res.send();
   }
 
   @Get('refresh')
