2731df04b710a9f23b1b1e046f6382b4da0ac588
liverforpresent <dailynsteady@gmail.com>
2025-05-07 16:34:18 +0900

    chore: jwt 환경변수 등록

--- src/shared/config/configuration.ts
+++ src/shared/config/configuration.ts
@@ -10,4 +10,16 @@ export default () => ({
     clientSecret: process.env.KAKAO_CLIENT_SECRET,
     redirectUri: process.env.KAKAO_REDIRECT_URI,
   },
+
+  // Jwt
+  jwt: {
+    access: {
+      secret: process.env.JWT_ACCESS_SECRET,
+      expiration: process.env.JWT_ACCESS_EXPIRATION,
+    },
+    refresh: {
+      secret: process.env.JWT_REFRESH_SECRET,
+      expiration: process.env.JWT_REFRESH_EXPIRATION,
+    },
+  },
 });
