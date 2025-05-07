8b85356b041631aca5129f01f9338b30091298f4
Kim Seon Woo <sunwoo0251@gmail.com>
2025-05-07 21:36:02 +0900

    feat: 행사 목록 조회 DTO 구현, API 경로 수정에 따른 repository 수정

---
+++ src/article/domain/entity/article.list.item.ts
@@ -0,0 +1,9 @@
+export interface ArticleListItem {
+  id: number;
+  title: string;
+  organization: string;
+  thumbnailPath: string;
+  scrap_count: number;
+  view_count: number;
+  tags: string[];
+}
