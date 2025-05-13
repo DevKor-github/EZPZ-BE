9f60a20d1e6125786613d5434831035fa5ac6d3e
liverforpresent <dailynsteady@gmail.com>
2025-05-13 16:38:21 +0900

    chore: user decorator 수정

--- src/shared/presentation/decorator/user.decorator.ts
+++ src/shared/presentation/decorator/user.decorator.ts
@@ -2,12 +2,12 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
 export interface UserPayload {
-  userId: number;
+  userId: string;
 }
 
 export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest<Request>();
-  const user = request.user;
+  const user = request.user as UserPayload;
 
   return user;
 });
