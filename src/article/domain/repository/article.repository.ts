0d0ca443693663afd058cde0a341c705e45962bc
sw_vi.xi <sunwoo0251@gmail.com>
2025-05-16 15:54:02 +0900

    feat: id로 행사 조회하는 findById 함수 원형 구현

--- src/article/domain/repository/article.repository.ts
+++ src/article/domain/repository/article.repository.ts
@@ -1,5 +1,6 @@
 import { Article } from '../entity/article';
 import { ArticleListItem } from '../entity/article.list.item';
+import { ArticleDetailDto } from '../../application/dto/article.detail.dto';
 
 export interface ArticleRepository {
   save(article: Article): Promise<void>;
@@ -8,6 +9,7 @@ export interface ArticleRepository {
     isFinished?: boolean;
     sort?: 'createdAt' | 'scrapCount' | 'viewCount';
   }): Promise<ArticleListItem[]>;
+  findById(id: string): Promise<ArticleDetailDto | null>;
 }
 
 export const ARTICLE_REPOSITORY = Symbol('ARTICLE_REPOSITORY');
