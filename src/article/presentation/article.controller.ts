35de21b0ed1887d782ae48c7a9435fdd3e67f969
sw_vi.xi <sunwoo0251@gmail.com>
2025-05-16 15:54:38 +0900

    feat: id로 행사 조회하는 컨트롤러 구현

--- src/article/presentation/article.controller.ts
+++ src/article/presentation/article.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { ArticleList } from 'src/article/application/get/article.list';
 import { ArticleFilterDto } from 'src/article/application/dto/article.filter.dto';
 import { ArticleListItem } from 'src/article/domain/entity/article.list.item';
+import { ArticleDetail } from 'src/article/application/get/article.detail';
+import { ArticleDetailDto } from 'src/article/application/dto/article.detail.dto';
 
 @Controller('article')
 export class ArticleController {
-  constructor(private readonly service: ArticleList) {}
+  constructor(
+    private readonly listService: ArticleList,
+    private readonly detailService: ArticleDetail,
+  ) {}
 
   @Get()
   async list(@Query() filter: ArticleFilterDto): Promise<ArticleListItem[]> {
-    return this.service.getList(filter);
+    return this.listService.getList(filter);
+  }
+
+  @Get(':id')
+  async detail(@Param('id') id: string): Promise<ArticleDetailDto> {
+    return this.detailService.getDetail(id);
   }
 }
