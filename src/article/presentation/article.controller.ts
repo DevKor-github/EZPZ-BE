5a857c110ba9ec04b632a077630f7642866bd1e7
sw_vi.xi <sunwoo0251@gmail.com>
2025-05-18 22:52:36 +0900

    feat: 행사 생성 POST 컨트롤러 구현

--- src/article/presentation/article.controller.ts
+++ src/article/presentation/article.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, UseInterceptors, Body, UploadedFiles } from '@nestjs/common';
 import { ArticleList } from 'src/article/application/get/article.list';
 import { ArticleFilterDto } from 'src/article/application/dto/article.filter.dto';
 import { ArticleListItem } from 'src/article/application/dto/article.list.item';
 import { ArticleDetail } from 'src/article/application/get/article.detail';
 import { ArticleDetailDto } from 'src/article/application/dto/article.detail.dto';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { ArticleCreate } from 'src/article/application/post/article.create';
+import { ArticleCreateDto } from 'src/article/application/dto/article.create.dto';
 
 @Controller('article')
 export class ArticleController {
   constructor(
     private readonly listService: ArticleList,
     private readonly detailService: ArticleDetail,
+    private readonly createService: ArticleCreate,
   ) {}
 
   @Get()
@@ -20,5 +24,23 @@ export class ArticleController {
   @Get(':id')
   async detail(@Param('id') id: string): Promise<ArticleDetailDto> {
     return this.detailService.getDetail(id);
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'thumbnail', maxCount: 1 },
+      { name: 'media', maxCount: 10 },
+    ]),
+  )
+  async create(
+    @Body() createDto: ArticleCreateDto,
+    @UploadedFiles()
+    files: {
+      thumbnail: Express.Multer.File;
+      media: Express.Multer.File[];
+    },
+  ) {
+    return this.createService.create(createDto, files);
   }
 }
