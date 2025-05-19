bb3bda6ea3bcd6a3cf786e32488d6b16de854f29
sw_vi.xi <sunwoo0251@gmail.com>
2025-05-19 19:27:48 +0900

    feat: 태그와 이미지 없이 POST 요청을 처리하는 부분까지 구현

--- src/article/presentation/article.controller.ts
+++ src/article/presentation/article.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, Query, Param, Post, UseInterceptors, Body, UploadedFiles } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
 import { ArticleList } from 'src/article/application/get/article.list';
 import { ArticleFilterDto } from 'src/article/application/dto/article.filter.dto';
 import { ArticleListItem } from 'src/article/application/dto/article.list.item';
 import { ArticleDetail } from 'src/article/application/get/article.detail';
 import { ArticleDetailDto } from 'src/article/application/dto/article.detail.dto';
-import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { ArticleCreate } from 'src/article/application/post/article.create';
 import { ArticleCreateDto } from 'src/article/application/dto/article.create.dto';
 
@@ -27,20 +26,8 @@ export class ArticleController {
   }
 
   @Post()
-  @UseInterceptors(
-    FileFieldsInterceptor([
-      { name: 'thumbnail', maxCount: 1 },
-      { name: 'media', maxCount: 10 },
-    ]),
-  )
-  async create(
-    @Body() createDto: ArticleCreateDto,
-    @UploadedFiles()
-    files: {
-      thumbnail: Express.Multer.File;
-      media: Express.Multer.File[];
-    },
-  ) {
-    return this.createService.create(createDto, files);
+  async create(@Body() createDto: ArticleCreateDto) {
+    console.log('Received createDto:', createDto);
+    return this.createService.create(createDto);
   }
 }
