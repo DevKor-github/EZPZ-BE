7e8a8b07b1a58d15eac677fdba0d581505a1cbf1
liverforpresent <dailynsteady@gmail.com>
2025-05-21 01:52:13 +0900

    feat: s3에 파일 업로드 로직 구현

---
+++ src/media/infrastructure/util/s3.adapter.ts
@@ -0,0 +1,49 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { v4 } from 'uuid';
+
+@Injectable()
+export class S3Adapter {
+  private readonly s3Client: S3Client;
+  private readonly bucketName: string;
+  private readonly bucketDomain: string;
+  // private readonly cloudFrontDomain: string;
+
+  constructor(private readonly configSerivce: ConfigService) {
+    this.s3Client = new S3Client({
+      region: this.configSerivce.getOrThrow('aws.region'),
+      credentials: {
+        accessKeyId: this.configSerivce.getOrThrow('aws.accessKeyId'),
+        secretAccessKey: this.configSerivce.getOrThrow('aws.secretAccessKey'),
+      },
+    });
+    this.bucketName = this.configSerivce.getOrThrow('aws.s3.bucketName');
+    this.bucketDomain = this.configSerivce.getOrThrow('aws.s3.bucketDomain');
+    // this.cloudFrontDomain = this.configSerivce.getOrThrow('aws.s3.cloudFrontDomain');
+  }
+
+  async upload(file: Express.Multer.File, articleId: string): Promise<string> {
+    const { originalname, buffer } = file;
+
+    const key = this.generateKey(originalname, articleId);
+
+    const params = {
+      Bucket: this.bucketName,
+      Key: key,
+      Body: buffer,
+      ContentType: file.mimetype,
+    };
+
+    await this.s3Client.send(new PutObjectCommand(params));
+
+    return `${this.bucketDomain}/${key}`;
+  }
+
+  private generateKey(originalname: string, articleId: string): string {
+    const extension = originalname.split('.').pop();
+    const uuid = v4();
+
+    return `images/${articleId}/${uuid}.${extension}`;
+  }
+}
