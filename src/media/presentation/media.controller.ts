4ac7eaacab2a2e4844cca9cc17ed66280e0bbaf6
liverforpresent <dailynsteady@gmail.com>
2025-05-30 10:43:15 +0900

    docs: media api swagger 문서 작성

--- src/media/presentation/media.controller.ts
+++ src/media/presentation/media.controller.ts
@@ -3,6 +3,7 @@ import { GeneratePresignedUrlUseCase } from '../application/generate-presigned-u
 import { GeneratePresignedUrlRequestDto } from '../application/generate-presigned-url/dto/generate-presigned-url.request.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { GeneratePresignedUrlResponseDto } from '../application/generate-presigned-url/dto/generate-presigned-url.response.dto';
+import { MediaDocs } from './media.docs';
 
 @ApiTags('media')
 @Controller('media')
@@ -10,7 +11,8 @@ export class MediaController {
   constructor(private readonly generatePresignedUrlUsecCase: GeneratePresignedUrlUseCase) {}
 
   @Post('presigned-url')
-  async upload(
+  @MediaDocs('presignedUrl')
+  async generatePresignedUrl(
     @Body() generatePresignedUrlRequestDto: GeneratePresignedUrlRequestDto,
   ): Promise<GeneratePresignedUrlResponseDto[]> {
     return await this.generatePresignedUrlUsecCase.execute(generatePresignedUrlRequestDto);
