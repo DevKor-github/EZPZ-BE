1b5eb10d06fa7c3c6b840edc87a39eef2d752262
liverforpresent <dailynsteady@gmail.com>
2025-05-31 23:49:32 +0900

    feat: scrap repository findByUserId 구현

--- src/scrap/domain/repository/scrap.repository.ts
+++ src/scrap/domain/repository/scrap.repository.ts
@@ -2,6 +2,7 @@ import { Scrap } from '../entity/scrap';
 
 export interface ScrapRepository {
   save(scrap: Scrap): Promise<void>;
+  findByUserId(userId: string): Promise<Scrap[]>;
 }
 
 export const SCRAP_REPOSITORY = Symbol('SCRAP_REPOSITORY');
--- src/scrap/infrastructure/repository/scrap.repository.impl.ts
+++ src/scrap/infrastructure/repository/scrap.repository.impl.ts
@@ -2,10 +2,17 @@ import { EntityRepository } from '@mikro-orm/mysql';
 import { Scrap } from 'src/scrap/domain/entity/scrap';
 import { ScrapRepository } from 'src/scrap/domain/repository/scrap.repository';
 import { ScrapMapper } from '../mapper/scrap.mapper';
+import { ScrapEntity } from '../orm-entity/scrap.entity';
 
-export class ScrapRepositoryImpl extends EntityRepository<Scrap> implements ScrapRepository {
+export class ScrapRepositoryImpl extends EntityRepository<ScrapEntity> implements ScrapRepository {
   async save(userScrap: Scrap): Promise<void> {
     const userScrapEntity = ScrapMapper.toEntity(userScrap);
     await this.em.persistAndFlush(userScrapEntity);
+  }
+
+  async findByUserId(userId: string): Promise<Scrap[]> {
+    const scrapEntities = await this.find({ user: { id: userId } }, { orderBy: { updatedAt: 'DESC' } });
+
+    return scrapEntities.map((entity) => ScrapMapper.toDomain(entity));
   }
 }
