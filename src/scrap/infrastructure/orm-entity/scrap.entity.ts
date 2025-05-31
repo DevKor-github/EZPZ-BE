2496ea5dc79d31a2e72cc3f465426c085068395c
liverforpresent <dailynsteady@gmail.com>
2025-05-31 23:50:42 +0900

    chore: scrap orm entity 디렉토리명 변경 및 cascade 옵션 추가

--- src/scrap/infrastructure/orm-entity/scrap.entity.ts
+++ src/scrap/infrastructure/orm-entity/scrap.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, ManyToOne } from '@mikro-orm/core';
+import { Cascade, Entity, ManyToOne } from '@mikro-orm/core';
 import { ArticleEntity } from 'src/article/infrastructure/orm-entity/article.entity';
 import { BaseEntity } from 'src/shared/infrastructure/orm-entity/base.entity';
 import { UserEntity } from '../../../user/infrastructure/orm-entity/user.entity';
@@ -6,9 +6,9 @@ import { ScrapRepositoryImpl } from '../repository/scrap.repository.impl';
 
 @Entity({ tableName: 'scrap', repository: () => ScrapRepositoryImpl })
 export class ScrapEntity extends BaseEntity {
-  @ManyToOne(() => ArticleEntity)
+  @ManyToOne(() => ArticleEntity, { cascade: [Cascade.REMOVE] })
   article: ArticleEntity;
 
-  @ManyToOne(() => UserEntity)
+  @ManyToOne(() => UserEntity, { cascade: [Cascade.REMOVE] })
   user: UserEntity;
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,7 +4,7 @@ import { USER_REPOSITORY } from './domain/repository/user.repository';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UserEntity } from './infrastructure/orm-entity/user.entity';
 import { CreateUserUseCase } from './application/create-user/create-user.use-case';
-import { ScrapEntity } from '../scrap/infrastructure/entity/scrap.entity';
+import { ScrapEntity } from '../scrap/infrastructure/orm-entity/scrap.entity';
 import { UserController } from './presentation/user.controller';
 import { GetMyInfoUseCase } from './application/get-my-info/get-my-info.use-case';
 import { DeleteMyInfoUseCase } from './application/delete-my-info/delete-my-info.use-case';
