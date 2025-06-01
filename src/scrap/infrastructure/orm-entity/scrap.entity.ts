4339b646b04e6d9a067bbf6561830c25155971e1
liverforpresent <dailynsteady@gmail.com>
2025-06-02 01:08:07 +0900

    chore: scrap orm-entity cascade 옵션 변경

--- src/scrap/infrastructure/orm-entity/scrap.entity.ts
+++ src/scrap/infrastructure/orm-entity/scrap.entity.ts
@@ -1,4 +1,4 @@
-import { Cascade, Entity, ManyToOne, Unique } from '@mikro-orm/core';
+import { Entity, ManyToOne, Unique } from '@mikro-orm/core';
 import { ArticleEntity } from 'src/article/infrastructure/orm-entity/article.entity';
 import { BaseEntity } from 'src/shared/infrastructure/orm-entity/base.entity';
 import { UserEntity } from '../../../user/infrastructure/orm-entity/user.entity';
@@ -7,9 +7,9 @@ import { ScrapRepositoryImpl } from '../repository/scrap.repository.impl';
 @Entity({ tableName: 'scrap', repository: () => ScrapRepositoryImpl })
 @Unique({ properties: ['article', 'user'] })
 export class ScrapEntity extends BaseEntity {
-  @ManyToOne(() => ArticleEntity, { cascade: [Cascade.REMOVE] })
+  @ManyToOne(() => ArticleEntity, { deleteRule: 'cascade' })
   article: ArticleEntity;
 
-  @ManyToOne(() => UserEntity, { cascade: [Cascade.REMOVE] })
+  @ManyToOne(() => UserEntity, { deleteRule: 'cascade' })
   user: UserEntity;
 }
