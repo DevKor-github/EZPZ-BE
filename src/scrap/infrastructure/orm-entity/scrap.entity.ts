95e3feafc7ffe8bdaf2817eb8c2995a28ef88528
liverforpresent <dailynsteady@gmail.com>
2025-06-01 14:47:12 +0900

    feat: scrap orm-entity article, user compositekey 설정

--- src/scrap/infrastructure/orm-entity/scrap.entity.ts
+++ src/scrap/infrastructure/orm-entity/scrap.entity.ts
@@ -1,10 +1,11 @@
-import { Cascade, Entity, ManyToOne } from '@mikro-orm/core';
+import { Cascade, Entity, ManyToOne, Unique } from '@mikro-orm/core';
 import { ArticleEntity } from 'src/article/infrastructure/orm-entity/article.entity';
 import { BaseEntity } from 'src/shared/infrastructure/orm-entity/base.entity';
 import { UserEntity } from '../../../user/infrastructure/orm-entity/user.entity';
 import { ScrapRepositoryImpl } from '../repository/scrap.repository.impl';
 
 @Entity({ tableName: 'scrap', repository: () => ScrapRepositoryImpl })
+@Unique({ properties: ['article', 'user'] })
 export class ScrapEntity extends BaseEntity {
   @ManyToOne(() => ArticleEntity, { cascade: [Cascade.REMOVE] })
   article: ArticleEntity;
