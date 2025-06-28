c315090f7e961a9c742da561860a5243e67960aa
liverforpresent <dailynsteady@gmail.com>
2025-06-28 12:46:44 +0900

    feat: scrap query 모듈 구현

---
+++ src/scrap/query/domain/scrap.model.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ScrapModel {
+  @ApiProperty({
+    example: '342342412123',
+    description: '게시글 ID',
+  })
+  articleId: string;
+
+  @ApiProperty({
+    example: '제목이얌',
+    description: '게시글 제목',
+  })
+  title: string;
+
+  @ApiProperty({
+    example: '고려대학교',
+    description: '주최자/기관',
+  })
+  organization: string;
+
+  @ApiProperty({
+    example: 6,
+    description: '조회수',
+  })
+  viewCount: number;
+
+  @ApiProperty({
+    example: 5,
+    description: '스크랩 수',
+  })
+  scrapCount: number;
+
+  @ApiProperty({
+    example: 'https://example.com/thumbnail.jpg',
+    description: '썸네일 이미지 경로',
+  })
+  thumbnailPath: string;
+
+  @ApiProperty({
+    example: 'tag1,tag2',
+    description: '태그 목록. 배열이 아닌, 문자열 형태로, 쉼표(,)로 띄어쓰기 없이 구분 됨',
+  })
+  tags: string | null;
+}
