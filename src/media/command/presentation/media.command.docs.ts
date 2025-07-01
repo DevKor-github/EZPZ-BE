2d6b4e8f98a0ab5e7d3f81fd6f8cf6bb1cc381c9
liverforpresent <dailynsteady@gmail.com>
2025-07-01 20:48:00 +0900

    docs: presignedUrl 작동 방식 기술

--- src/media/command/presentation/media.command.docs.ts
+++ src/media/command/presentation/media.command.docs.ts
@@ -1,10 +1,10 @@
 import { applyDecorators } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
+  ApiCreatedResponse,
   ApiForbiddenResponse,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
-  ApiOkResponse,
   ApiOperation,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
@@ -18,9 +18,26 @@ export const MediaCommandDocs = createDocs<MediaCommandEndpoint>({
     applyDecorators(
       ApiOperation({
         summary: 'Presigned URL 생성',
-        description: 'S3에 파일 업로드를 위한 Presigned URL을 생성합니다.',
+        description: `
+        ★ 게시글 생성 API 흐름
+
+        1. POST /article 요청 => articleId 받음
+
+        2. POST /media/presigned-url 
+            => S3에 파일 업로드를 위한 Presigned URL을 생성
+            => [{presignedUrl, objectKey}] 받음
+            => thumbnail과 일반 이미지의 presignedUrl이 구분되어 있음
+        
+        3. PUT https://presigendUrl~~~
+            => 2번에서 받은 presignedUrl을 통해 파일 업로드
+            => 성공 시 200 OK 응답
+        
+        4. POST /media 요청
+            => presignedUrl에서 받은 objectKey를 포함하여 미디어 메타데이터 저장
+            => 성공 시 201 Created 응답
+        `,
       }),
-      ApiOkResponse({
+      ApiCreatedResponse({
         description: 'Presigned URL이 성공적으로 생성됨',
         type: GeneratePresignedUrlResponseDto,
       }),
@@ -46,7 +63,7 @@ export const MediaCommandDocs = createDocs<MediaCommandEndpoint>({
         summary: '미디어 생성',
         description: 'S3에 파일을 업로드하고 메타데이터를 저장합니다.',
       }),
-      ApiOkResponse({
+      ApiCreatedResponse({
         description: '미디어가 성공적으로 생성됨',
       }),
       ApiBadRequestResponse({
