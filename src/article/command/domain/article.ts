c1dc13e0d47a07b32f9dd6ddbfbbec2f821abdf5
sw_vi.xi <sunwoo0251@gmail.com>
2025-07-08 09:17:59 +0900

    feat: update의 body로 넣어준 내용이 현재와 동일한 것인지 판단하는 조건 추가

--- src/article/command/domain/article.ts
+++ src/article/command/domain/article.ts
@@ -142,25 +142,25 @@ export class Article extends BaseDomainEntity<ArticleProps> {
     startAt?: Date;
     endAt?: Date;
   }): void {
-    if (props.title !== undefined) {
+    if (props.title !== undefined && props.title !== this.props.title) {
       this.props.title = props.title;
     }
-    if (props.organization !== undefined) {
+    if (props.organization !== undefined && props.organization !== this.props.organization) {
       this.props.organization = props.organization;
     }
-    if (props.location !== undefined) {
+    if (props.location !== undefined && props.location !== this.props.location) {
       this.props.location = props.location;
     }
-    if (props.description !== undefined) {
+    if (props.description !== undefined && props.description !== this.props.description) {
       this.props.description = props.description;
     }
-    if (props.registrationUrl !== undefined) {
+    if (props.registrationUrl !== undefined && props.registrationUrl !== this.props.registrationUrl) {
       this.props.registrationUrl = props.registrationUrl;
     }
-    if (props.startAt !== undefined) {
+    if (props.startAt !== undefined && props.startAt !== this.props.startAt) {
       this.props.startAt = props.startAt;
     }
-    if (props.endAt !== undefined) {
+    if (props.endAt !== undefined && props.endAt !== this.props.endAt) {
       this.props.endAt = props.endAt;
     }
 
