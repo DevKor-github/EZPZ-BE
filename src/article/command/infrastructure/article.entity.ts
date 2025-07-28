77848a4ff6276e72c075ef6c18e312fd7d98e10e
liverforpresent <dailynsteady@gmail.com>
2025-07-28 17:19:53 +0900

    fix: article description 최대 길이 변경

--- src/article/command/infrastructure/article.entity.ts
+++ src/article/command/infrastructure/article.entity.ts
@@ -14,7 +14,7 @@ export class ArticleEntity extends BaseEntity {
   @Property({ type: 'varchar' })
   location: string;
 
-  @Property({ type: 'varchar', length: 1024 })
+  @Property({ type: 'varchar', length: 2047 })
   description: string;
 
   @Property({ type: 'varchar' })
