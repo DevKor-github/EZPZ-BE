0ae12ff5e30b82107156c442b4caa21dc4bae3c9
liverforpresent <dailynsteady@gmail.com>
2025-10-15 20:40:06 +0900

    fix: auth 앰플리튜드 이벤트 핸들러 클래스별로 분리 및 이벤트명 수정

--- src/analytics/application/auth-event.handler.ts
+++ src/analytics/application/auth-event.handler.ts
@@ -3,20 +3,23 @@ import { AuthCreatedEvent } from 'src/auth/domain/event/auth-created.event';
 import { AnalyticsService } from '../infrastructure/analytics.service';
 import { LoginSucceededEvent } from 'src/auth/domain/event/login-succeeded.event';
 
-@EventsHandler(AuthCreatedEvent, LoginSucceededEvent)
-export class AuthEventsHandler implements IEventHandler<AuthCreatedEvent | LoginSucceededEvent> {
+@EventsHandler(AuthCreatedEvent)
+export class AuthCreatedEventHandler implements IEventHandler<AuthCreatedEvent> {
   constructor(private readonly analyticsService: AnalyticsService) {}
 
-  handle(event: AuthCreatedEvent | LoginSucceededEvent) {
-    if (event instanceof AuthCreatedEvent) {
-      this.analyticsService.trackEvent(event.userId.value, 'signup_success', {
-        email: event.email,
-        role: event.role,
-      });
-    }
+  handle(event: AuthCreatedEvent) {
+    this.analyticsService.trackEvent(event.userId.value, 'Signed Up', {
+      email: event.email,
+      role: event.role,
+    });
+  }
+}
 
-    if (event instanceof LoginSucceededEvent) {
-      this.analyticsService.trackEvent(event.userId.value, 'login_success');
-    }
+@EventsHandler(LoginSucceededEvent)
+export class LoginSucceededEventHandler implements IEventHandler<LoginSucceededEvent> {
+  constructor(private readonly analyticsService: AnalyticsService) {}
+
+  handle(event: LoginSucceededEvent) {
+    this.analyticsService.trackEvent(event.userId.value, 'Logged In', {});
   }
 }
