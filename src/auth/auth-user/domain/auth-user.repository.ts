f1fa22abb876a4f77e016afba33bb4df2a4085c5
liverforpresent <dailynsteady@gmail.com>
2025-11-02 19:55:23 +0900

    feat: auth repository에 deleteById 구현

--- src/auth/auth-user/domain/auth-user.repository.ts
+++ src/auth/auth-user/domain/auth-user.repository.ts
@@ -6,6 +6,7 @@ export interface AuthUserRepository {
   findByOAuthIdandProvider(oauthId: string, provider: string): Promise<AuthUser | null>;
   findByRefreshToken(refreshToken: string): Promise<AuthUser | null>;
   findByUserId(userId: string): Promise<AuthUser | null>;
+  deleteById(id: string): Promise<void>;
 }
 
 export const AUTH_USER_REPOSITORY = Symbol('AUTH_USER_REPOSITORY');
--- src/auth/auth-user/infrasturcture/auth-user.repository.impl.ts
+++ src/auth/auth-user/infrasturcture/auth-user.repository.impl.ts
@@ -5,6 +5,8 @@ import { InjectRepository } from '@mikro-orm/nestjs';
 import { AuthUserRepository } from 'src/auth/auth-user/domain/auth-user.repository';
 import { AuthUserEntity } from './auth-user.entity';
 import { AuthUserMapper } from './auth-user.mapper';
+import { CustomException } from 'src/shared/exception/custom-exception';
+import { CustomExceptionCode } from 'src/shared/exception/custom-exception-code';
 
 export class AuthUserRepositoryImpl implements AuthUserRepository {
   constructor(
@@ -12,7 +14,6 @@ export class AuthUserRepositoryImpl implements AuthUserRepository {
     private readonly authOrmRepository: EntityRepository<AuthUserEntity>,
     private readonly em: EntityManager,
   ) {}
-
   async save(authUser: AuthUser): Promise<void> {
     const authUserEntity = AuthUserMapper.toEntity(authUser, this.em);
     await this.em.persistAndFlush(authUserEntity);
@@ -48,5 +49,12 @@ export class AuthUserRepositoryImpl implements AuthUserRepository {
     if (!authUserEntity) return null;
 
     return AuthUserMapper.toDomain(authUserEntity);
+  }
+
+  async deleteById(id: string): Promise<void> {
+    const authUserEntity = await this.authOrmRepository.findOne({ id });
+    if (!authUserEntity) throw new CustomException(CustomExceptionCode.AUTH_USER_NOT_FOUND);
+
+    await this.em.removeAndFlush(authUserEntity);
   }
 }
