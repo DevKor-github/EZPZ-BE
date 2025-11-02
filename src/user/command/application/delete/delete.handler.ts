577651c8234ffb7949f1b0b11e1e2e654bc49ae4
liverforpresent <dailynsteady@gmail.com>
2025-11-02 19:59:31 +0900

    fix: 일반 유저 정보 삭제 시 oAuth 로직을 직접 사용하지 않도록 수정

--- src/user/command/application/delete/delete.handler.ts
+++ src/user/command/application/delete/delete.handler.ts
@@ -1,6 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { OAuthProviderFactory } from 'src/auth/core/infrastructure/oauth/oauth-provider.factory';
-import { OAuthProviderType } from 'src/auth/auth-user/domain/value-object/oauth-provider.enum';
 import { USER_COMMAND_REPOSITORY, UserCommandRepository } from '../../domain/user.command.repository';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { DeleteMyInfoCommand } from './delete.command';
@@ -11,14 +9,10 @@ export class DeleteMyInfoHandler implements ICommandHandler<DeleteMyInfoCommand>
   constructor(
     @Inject(USER_COMMAND_REPOSITORY)
     private readonly userCommandRepository: UserCommandRepository,
-    private readonly oAuthProviderFactory: OAuthProviderFactory,
   ) {}
 
   async execute(command: DeleteMyInfoCommand): Promise<void> {
     const { userId } = command;
-    const oAuthProvider = this.oAuthProviderFactory.getProvider(OAuthProviderType.KAKAO);
-    console.log(1);
-    await oAuthProvider.unlinkAccount(userId);
 
     await this.userCommandRepository.deleteById(userId);
   }
