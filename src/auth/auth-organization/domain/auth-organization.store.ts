85f985c628a866025ca2203507b8e348bbe7ee62
liverforpresent <dailynsteady@gmail.com>
2025-11-04 23:00:17 +0900

    feat: AuthOrganization repository 구현

---
+++ src/auth/auth-organization/domain/auth-organization.store.ts
@@ -0,0 +1,11 @@
+import { AuthOrganization } from './auth-organization';
+
+export interface AuthOrganizationStore {
+  save(authOrganization: AuthOrganization): Promise<void>;
+  update(authOrganization: AuthOrganization): Promise<void>;
+  loadByOrganizationId(organizationId: string): Promise<AuthOrganization | null>;
+  loadByRefreshToken(refreshToken: string): Promise<AuthOrganization | null>;
+  loadByAccountId(accountId: string): Promise<AuthOrganization | null>;
+}
+
+export const AUTH_ORGANIZATION_STORE = Symbol('AUTH_ORGANIZATION_STORE');
---
+++ src/auth/auth-organization/infrastructure/auth-organization.store.impl.ts
@@ -0,0 +1,51 @@
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { AuthOrganization } from '../domain/auth-organization';
+import { AuthOrganizationStore } from '../domain/auth-organization.store';
+import { AuthOrganizationEntity } from './auth-organization.entity';
+import { EntityManager, EntityRepository } from '@mikro-orm/mysql';
+import { AuthOrganizationMapper } from './auth-organization.mapper';
+
+export class AuthOrganizationStoreImpl implements AuthOrganizationStore {
+  constructor(
+    @InjectRepository(AuthOrganizationEntity)
+    private readonly ormRepository: EntityRepository<AuthOrganizationEntity>,
+    private readonly em: EntityManager,
+  ) {}
+
+  async save(authOrganization: AuthOrganization): Promise<void> {
+    const authOrganizationEntity = AuthOrganizationMapper.toPersistence(authOrganization);
+    await this.em.persistAndFlush(authOrganizationEntity);
+  }
+
+  async update(authOrganization: AuthOrganization): Promise<void> {
+    await this.em.nativeUpdate(
+      AuthOrganizationEntity,
+      { id: authOrganization.id.value },
+      {
+        refreshToken: authOrganization.refreshToken,
+        updatedAt: authOrganization.updatedAt,
+      },
+    );
+  }
+
+  async loadByOrganizationId(organizationId: string): Promise<AuthOrganization | null> {
+    const authOrganizationEntity = await this.ormRepository.findOne({ organizationId });
+    if (authOrganizationEntity == null) return null;
+
+    return AuthOrganizationMapper.toDomain(authOrganizationEntity);
+  }
+
+  async loadByRefreshToken(refreshToken: string): Promise<AuthOrganization | null> {
+    const authOrganizationEntity = await this.ormRepository.findOne({ refreshToken });
+    if (authOrganizationEntity == null) return null;
+
+    return AuthOrganizationMapper.toDomain(authOrganizationEntity);
+  }
+
+  async loadByAccountId(accountId: string): Promise<AuthOrganization | null> {
+    const authOrganizationEntity = await this.ormRepository.findOne({ accountId });
+    if (authOrganizationEntity == null) return null;
+
+    return AuthOrganizationMapper.toDomain(authOrganizationEntity);
+  }
+}
