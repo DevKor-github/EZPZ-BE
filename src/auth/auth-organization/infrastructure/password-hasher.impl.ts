e521d3cd1c3c5fb03fb135895442ed6121184148
liverforpresent <dailynsteady@gmail.com>
2025-11-04 22:59:50 +0900

    feat: 비밀번호 hash 관련 기능 구현

---
+++ src/auth/auth-organization/infrastructure/password-hasher.impl.ts
@@ -0,0 +1,12 @@
+import * as bcrypt from 'bcrypt';
+import { PasswordHasher } from '../domain/password-hasher';
+
+export class PasswordHasherImpl implements PasswordHasher {
+  async hash(plain: string): Promise<string> {
+    return await bcrypt.hash(plain, 10);
+  }
+
+  async compare(plain: string, hashed: string): Promise<boolean> {
+    return await bcrypt.compare(plain, hashed);
+  }
+}
