e8290013b09139dc54bd43163b19006c1fbe3b29
liverforpresent <dailynsteady@gmail.com>
2025-11-09 21:36:11 +0900

    docs: organization swagger 문서 작성

--- src/organization/command/presentation/organization.command.controller.ts
+++ src/organization/command/presentation/organization.command.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Patch, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UpdateOrganizationUseCase } from '../application/update-organization/update-organization.use-case';
 import { UpdateOrganizationDto } from './dto/request/update-organization.request.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User, UserPayload } from 'src/shared/core/presentation/user.decorator';
+import { OrganizationCommandDocs } from './organization.command.docs';
 
 @ApiTags('organization')
 @Controller('organization')
 export class OrganizationCommandController {
   constructor(private readonly updateOrganizationUseCase: UpdateOrganizationUseCase) {}
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() dto: UpdateOrganizationDto): Promise<void> {
+  @Patch()
+  @UseGuards(AuthGuard('jwt-access'))
+  @OrganizationCommandDocs('update')
+  async update(@User() user: UserPayload, @Body() dto: UpdateOrganizationDto): Promise<void> {
     await this.updateOrganizationUseCase.execute({
-      organizationId: id,
+      organizationId: user.userId,
       name: dto.name,
       contact: dto.contact,
     });
