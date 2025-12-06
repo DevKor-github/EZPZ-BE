4dccadc86a5ac2ffd463620f9649f723ac19b70c
liverforpresent <dailynsteady@gmail.com>
2025-12-06 16:07:55 +0900

    fix: 유저 엔티티에서 Role column 제거

--- iam/user/infrastructure/user.entity.ts
+++ iam/user/infrastructure/user.entity.ts
@@ -1,15 +1,11 @@
 import { Cascade, Collection, Entity, OneToMany, Property } from '@mikro-orm/core';
 import { ScrapEntity } from 'src/scrap/command/infrastructure/scrap.entity';
 import { BaseEntity } from 'src/shared/core/infrastructure/orm-entity/base.entity';
-import { Role } from '../domain/value-object/role.enum';
 
 @Entity({ tableName: 'user' })
 export class UserEntity extends BaseEntity {
   @Property({ type: 'varchar', unique: true })
   email: string;
-
-  @Property({ type: 'varchar' })
-  role: Role;
 
   @OneToMany(() => ScrapEntity, (scrap) => scrap.user, { nullable: true, cascade: [Cascade.ALL] })
   scraps = new Collection<ScrapEntity>(this);
