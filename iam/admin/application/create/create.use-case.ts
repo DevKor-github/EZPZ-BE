42e8fe9f17092dbe8198d8d743a12df3dc336cfe
liverforpresent <dailynsteady@gmail.com>
2025-12-19 19:22:27 +0900

    feat: admin 생성 로직 구현

---
+++ iam/admin/application/create/create.use-case.ts
@@ -0,0 +1,29 @@
+import { ADMIN_REPOSITORY, AdminRepository } from 'iam/admin/domain/admin.repository';
+import { CreateAdminCommand } from './create.command';
+import { CreateAdminResult } from './create.result';
+import { Inject, Injectable } from '@nestjs/common';
+import { Admin } from 'iam/admin/domain/admin';
+import { Identifier } from 'src/shared/core/domain/identifier';
+
+@Injectable()
+export class CreateAdminUseCase {
+  constructor(
+    @Inject(ADMIN_REPOSITORY)
+    private readonly adminRepository: AdminRepository,
+  ) {}
+
+  async execute(command: CreateAdminCommand): Promise<CreateAdminResult> {
+    const { name } = command;
+
+    const admin = Admin.create({
+      id: Identifier.create(),
+      name,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await this.adminRepository.save(admin);
+
+    return { adminId: admin.id.value };
+  }
+}
