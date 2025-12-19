f71604fb02b85f42eb3af737d2dc588ed97043bb
liverforpresent <dailynsteady@gmail.com>
2025-12-19 19:24:48 +0900

    chore: admin 테이블명 변경

--- iam/admin/infrastructure/admin.entity.ts
+++ iam/admin/infrastructure/admin.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Property } from '@mikro-orm/core';
 import { BaseEntity } from 'src/shared/core/infrastructure/orm-entity/base.entity';
 
-@Entity()
+@Entity({ tableName: 'admin' })
 export class AdminEntity extends BaseEntity {
   @Property({ type: 'varchar' })
   name: string;
